d1ad8cbf7da7e5135cfb3f8dfa601772644d986a
Josiah Sam <jsam@localhost.localdomain>
2020-10-14 13:42:01 -0400

    SRC-1.1.6: Adding resources directory.

--- modules/ControlGUI/src/main.cpp
+++ modules/ControlGUI/src/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile qss("../style.qss");
+    QFile qss("../resources/style.qss");
     qss.open(QFile::ReadOnly);
     a.setStyleSheet(qss.readAll());
     qss.close();
